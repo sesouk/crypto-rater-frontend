31a99953437701675e85299e6e64493a0a8846da
Kevin Tran <sesouk@gmail.com>
2021-10-16 20:07:21 -0600

    added plus minus functionality

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React, { useEffect, useState } from "react";
 import { ethers } from "ethers";
 import './App.css';
+import abi from './utils/CryptoRater.json'
 
 export default function App() {
   const [currentAccount, setCurrentAccount] = useState("")
+  const [plusCount, setPlus] = useState("")
+  const [minusCount, setMinus] = useState("")
+
+  const contractAddress = "0xF6E6471D94d7B51B769f7804e3F1732b6020cf69"
+
+  const contractABI = abi.abi
 
   const checkIfWalletIsConnected = async () => {
     try {
@@ -30,6 +37,27 @@ export default function App() {
   }
 }
 
+const getCounts = async () => {
+  try {
+    const { ethereum } = window
+
+    const provider = new ethers.providers.Web3Provider(ethereum)
+    const signer = provider.getSigner()
+    const cryptoRateContract = new ethers.Contract(contractAddress, contractABI, signer)
+
+    let plusCount = await cryptoRateContract.getPlus()
+    console.log("Received thumbs up count...", plusCount.toNumber());
+
+    let minusCount = await cryptoRateContract.getMinus()
+    console.log("Received thumbs down count...", minusCount.toNumber());
+
+    setPlus(plusCount.toNumber())
+    setMinus(minusCount.toNumber())
+  } catch (error) {
+    console.log(error);
+  }
+}
+
 const connectWallet = async () => {
   try {
     const { ethereum } = window
@@ -48,16 +76,70 @@ const connectWallet = async () => {
   }
 }
 
-  const plus = () => {
-    
+  const plus = async () => {
+    try {
+      const { ethereum } = window
+      
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum)
+        const signer = provider.getSigner()
+        const cryptoRateContract = new ethers.Contract(contractAddress, contractABI, signer)
+
+        let plusCount = await cryptoRateContract.getPlus()
+        console.log("Received thumbs up count...", plusCount.toNumber());
+
+        const plusTxn = await cryptoRateContract.plus()
+        console.log("Mining...", plusTxn.hash);
+
+        await plusTxn.wait()
+        console.log("Mined -- ", plusTxn.hash);
+
+        plusCount = await cryptoRateContract.getPlus()
+        console.log("Received thumbs up count...", plusCount.toNumber());
+
+        setPlus(plusCount.toNumber())
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log(error);
+    }
   }
 
-  const minus = () => {
-    
+  const minus = async () => {
+    try {
+      const { ethereum } = window
+      
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum)
+        const signer = provider.getSigner()
+        const cryptoRateContract = new ethers.Contract(contractAddress, contractABI, signer)
+
+        let minusCount = await cryptoRateContract.getMinus()
+        console.log("Received thumbs down count...", minusCount.toNumber());
+
+        const minusTxn = await cryptoRateContract.minus()
+        console.log("Mining...", minusTxn.hash);
+
+        await minusTxn.wait()
+        console.log("Mined -- ", minusTxn.hash);
+
+        minusCount = await cryptoRateContract.getMinus()
+        console.log("Received thumbs up count...", minusCount.toNumber());
+
+        setMinus(minusCount.toNumber())
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log(error);
+    }
   }
+
 
   useEffect(() => {
     checkIfWalletIsConnected()
+    getCounts()
   }, [])
   
   return (
@@ -72,6 +154,9 @@ const connectWallet = async () => {
           My name's Kevin and I'll be collecting your votes on how you feel about crypto. We'll start with just ETH and I'll continually add more options for you to vote on. Connect your wallet and place some votes!
         <div className="eth">
           How do you feel about ETH?
+        </div>
+        <div className="eth">
+          Total thumbs up: {plusCount} Total thumbs down: {minusCount}
         </div>
         </div>
         <div className="btnContainer">
